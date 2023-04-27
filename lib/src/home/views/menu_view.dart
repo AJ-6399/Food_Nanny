19ba92c5840ce94e530863ca7ad2f334a330d81d
AJ-6399 <128492801+AJ-6399@users.noreply.github.com>
2023-04-27 15:07:32 +0100

    27-4-23

--- lib/src/home/views/menu_view.dart
+++ lib/src/home/views/menu_view.dart
@@ -15,6 +15,19 @@ class MenuView extends GetView<HomeController> {
   int c = 0;
   @override
   Widget build(BuildContext context) {
+    final reference_data = FirebaseFirestore.instance
+        .collection('chef_orders')
+        .doc(currCook)
+        .collection('active_menu');
+    var data_id;
+    List<String> id_list = [];
+    dataDelete(int Index) {
+      var doc_ref = id_list[Index];
+      final is_Deleted = reference_data.doc(doc_ref).delete();
+      id_list.removeAt(Index);
+      return is_Deleted;
+    }
+
     return Scaffold(
       body: WillPopScope(
         onWillPop: () async => false,
@@ -22,40 +35,43 @@ class MenuView extends GetView<HomeController> {
           height: 600,
           width: double.infinity,
           child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('chef_orders')
-                .doc(currCook)
-                .collection('active_menu')
-                .snapshots(),
+            stream: reference_data.snapshots(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (snapshot.hasData) {
                 print('has data');
                 return ListView.builder(
                     itemCount: snapshot.data!.docs.length,
                     itemBuilder: (context, index) {
+                      data_id = snapshot.data!.docs[index].id;
+                      if (id_list.length <= snapshot.data!.docs.length) {
+                        id_list.add(data_id);
+                      }
+
+                      print(id_list);
                       foodtitle = snapshot.data!.docs[index]['foodTitle'];
                       foodPrice = snapshot.data!.docs[index]['foodPrice'];
                       foodingredients =
                           snapshot.data!.docs[index]['foodIngredients'];
                       return Card(
                         child: ListTile(
-                          title: Text(foodtitle!,
-                              style: GoogleFonts.amaranth(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
-                                  fontSize: 24)),
-                          leading: const CircleAvatar(
-                            radius: 28,
-                            backgroundImage: AssetImage('images/french.jpg'),
-                          ),
-                          trailing: Text(
-                            foodPrice!,
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
+                            title: Text(foodtitle!,
+                                style: GoogleFonts.amaranth(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                    fontSize: 24)),
+                            leading: const CircleAvatar(
+                              radius: 28,
+                              backgroundImage: AssetImage('images/french.jpg'),
+                            ),
+                            trailing: InkWell(
+                              onTap: () {
+                                dataDelete(index);
+                              },
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            )),
                       );
                     });
               } else {
